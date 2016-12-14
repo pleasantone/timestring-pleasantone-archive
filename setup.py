17c7aa2f18141bf9f14d74db9f39414e58f81892
Paul Traina <bulk+github@pst.org>
2016-12-13 20:02:05 -0800

    Prepare for fork publication

--- setup.py
+++ setup.py
@@ -1,30 +1,54 @@
-#!/usr/bin/env python
-from setuptools import setup
-from timestring import version
+import os
+import sys
+from setuptools import setup, find_packages
 
-classifiers = ["Development Status :: 5 - Production/Stable",
-               "License :: OSI Approved :: Apache Software License",
-               "Programming Language :: Python",
-               "Programming Language :: Python :: 2.6",
-               "Programming Language :: Python :: 2.7",
-               "Programming Language :: Python :: 3.3",
-               "Programming Language :: Python :: 3.4",
-               "Programming Language :: Python :: 3.5",
-               "Programming Language :: Python :: Implementation :: PyPy"]
+from timestring import __version__
 
-setup(name='timestring-pst',
-      version=version,
-      description="Human expressed time to Dates and Ranges",
-      long_description="""Converting strings of into representable time via Date and Range objects.
- Plus features to compare and adjust Dates and Ranges.""",
-      classifiers=classifiers,
-      keywords='date time range datetime datestring',
-      author='@stevepeak',
-      author_email='steve@stevepeak.net',
-      url='http://github.com/stevepeak/timestring',
-      license='http://www.apache.org/licenses/LICENSE-2.0',
-      packages=['timestring'],
-      include_package_data=True,
-      zip_safe=True,
-      install_requires=["pytz>=2015.2"],
-      entry_points={'console_scripts': ['timestring=timestring:main']})
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "License :: OSI Approved :: Apache Software License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: Implementation :: PyPy"
+]
+
+setup(
+    name="timestring-pleasantone",
+    version=__version__,
+    description="Natural language date and date-range parsing (fork stevepeak)",
+    long_description=long_description,
+    classifiers=classifiers,
+    keywords="date time range datetime datestring",
+    author="@stevepeak, @paul_traina",
+    author_email="steve@stevepeak.net, bulk+github@pst.org",
+    url="http://github.com/pleasantone/timestring",
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    packages=find_packages(exclude=["examples", "tests"]),
+    include_package_data=True,
+    zip_safe=True,
+    entry_points={
+        "console_scripts": [
+            "timestring=timestring:main"
+        ]
+    },
+    install_requires=[
+        "pytz>=2015.2",
+    ],
+    tests_require=[
+        "nose",
+        "rednose",
+        "nose-cov",
+        "codecov",
+        "ddt",
+        "six",
+    ],
+    test_suite="nose.collector",
+)

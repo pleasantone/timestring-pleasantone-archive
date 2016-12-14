3c8417f035e09171d6566c5f43e97185e36a2675
Paul Traina <bulk+github@pst.org>
2016-12-13 17:48:45 -0800

    Import version from init

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from setuptools import setup
+from timestring import version
 
-version = '1.6.2.2'
 classifiers = ["Development Status :: 5 - Production/Stable",
                "License :: OSI Approved :: Apache Software License",
                "Programming Language :: Python",
